9236e51b5e84496663d99684f9f365fb32563123
Marce35 <markonedelkovski2002@gmail.com>
2023-11-08 12:03:58 +0100

    Initial commit

---
+++ Legacy_Explorer/src/main/java/mk/ukim/finki/legacy_explorer/LegacyExplorerApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.legacy_explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegacyExplorerApplication.class, args);
+    }
+
+}
